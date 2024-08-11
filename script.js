8bdb5681adf5a789a438832e0d9b9423fbf97562
gabioliveira0 <162706091+gabioliveira0@users.noreply.github.com>
2024-08-10 21:29:13 -0300

    x

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function mostraPergunta() {
 }
 
 function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas) {
+    for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa;
         caixaAlternativas.appendChild(botaoAlternativas);
